5b40fe0e18fe84052ce6140c4352702b505eb169
Pedro <pedrohenriquemateussi@gmail.com>
2026-09-02 01:07:06 -0300

    Adiciona desafio 3 de condicionais

---
+++ 02-condicionais/desafio02_maior_numero.py
@@ -0,0 +1,11 @@
+primeironumero = int(input("Digite um número: "))
+segundonumero = int(input("Digite um número: "))
+
+if primeironumero > segundonumero:
+    print(primeironumero, "É maior")
+
+elif primeironumero < segundonumero:
+    print(segundonumero, "É maior")
+
+else:
+    print("Os numeros são iguais")
