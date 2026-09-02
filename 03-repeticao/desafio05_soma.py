0fbd41e28dd9f6d09a57518af7d3969b3ab19c65
Pedro <pedrohenriquemateussi@gmail.com>
2026-09-02 00:36:54 -0300

    Adiciona desafio 5 - soma

---
+++ 03-repeticao/desafio05_soma.py
@@ -0,0 +1,10 @@
+numero = int(input("Digite um número: "))
+
+numeroinicial = 1
+soma = 0
+
+while numeroinicial <= numero:
+    soma = soma + numeroinicial
+    numeroinicial = numeroinicial + 1
+
+print(soma)
