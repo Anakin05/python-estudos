64fc71ec7a6213bbb323cd39af4585e58e59e3c8
Pedro <pedrohenriquemateussi@gmail.com>
2026-09-02 00:24:04 -0300

    Adiciona exercício de intervalo

---
+++ 03-repeticao/desafio04_exercicio_intervalo.py
@@ -0,0 +1,8 @@
+numeroinicial = int(input("Digite um número: "))
+numerofinal = int(input("Digite um número: "))
+
+contador = numeroinicial
+
+while contador <= numerofinal:
+    print(contador)
+    contador = contador + 1
