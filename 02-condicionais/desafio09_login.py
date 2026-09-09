bb488a1d8105a985412f39231bd99d9db70631e0
Pedro <pedrohenriquemateussi@gmail.com>
2026-09-09 00:16:46 -0300

    Adiciona desafio 9 de condicionais

---
+++ 02-condicionais/desafio09_login.py
@@ -0,0 +1,7 @@
+usuario = input("Digite o usuário: ")
+senha = input("Digite a senha: ")
+
+if usuario == "admin" and senha == "1234":
+    print("Login realizado!")
+else:
+    print("Usuário ou senha incorretos!")
