311b98df33f4e3e57fcaf9fa8c89c399a96792f6
Pedro <pedrohenriquemateussi@gmail.com>
2026-09-09 00:38:21 -0300

    Adiciona desafio 12

---
+++ 02-condicionais/desafio12_ficha_cliente.py
@@ -0,0 +1,5 @@
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+cidade = input("Digite sua cidade: ")
+
+print(f"Olá, {nome}! Você tem {idade} anos e mora em {cidade}.")
